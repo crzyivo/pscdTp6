68a10677a743b4162eb7668ee62896eb9ac69918
rankern <victorm.lafuente@gmail.com>
2018-01-02 16:23:48 +0100

    subasta

--- src/subastador.cpp
+++ src/subastador.cpp
@@ -2,6 +2,15 @@
 #include <string>
 #include <thread>
 #include "Socket.hpp"
+#include "monitorSubasta.hpp"
+/***********************
+ * Mensajes Servidor Cliente:
+ * "Comienza la subasta en %d\n"
+ * "Puja cliente %d aceptada. Quien ofrece %d\n" 
+ * "Puja no valida. Actual puja %d %d \n", cliente, puja
+ **********************/ 
+const int maxNumCLientes = 35;
+monitorSubasta global();
 
 int puertoSubasta = 32005;
 
@@ -10,14 +19,14 @@ void subastaCliente(Socket& subasta, int cliente){
 		string mensajeOut = "";
 		string mensajeIn = "";
 		int precioInicial = global.comenzarSubasta();
-		mensajeOut = "Comienza la subasta en " + to_string(precioInical) + "\n";
+		mensajeOut = "Comienza la subasta en " + to_string(precioInicial) + "\n";
 		subasta.Send(cliente, mensajeOut);
 		//esperar a que se dé la condición de iniciar subasta
-		while(/*Condición de fin subasta Actual*/){
+		while(1/*Condición de fin subasta Actual*/){
 			subasta.Recv(cliente, mensajeIn);
 			//leer mensaje
-			if(global.pujar(pujaCliente)){
-				mensajeOut = "Puja cliente " + to_string(cliente) + "aceptada. Quien ofrece\n" + to_string(global.pujaActual()+10) + "\n";
+			if(global.pujar(pujaCliente, cliente)){
+				mensajeOut = "Puja cliente " + to_string(cliente) + " aceptada. Quien ofrece\n" + to_string(global.pujaActual()+10) + "\n";
 			}else{
 				mensajeOut = "Puja no valida. Actual puja " + to_string(global.PujadorActual()) + to_string(global.pujaActual()) + "\n";
 			}
@@ -36,9 +45,9 @@ int main(int argc, char * argv[]){
 	Socket subasta(puertoSubasta);
 	
 	int sockSubasta = subasta.Bind();
-	int error = subasta.Listen();
+	int error = subasta.Listen(maxNumCLientes);
 
-	while(/*Condición fin de todas las subastas*/){
+	while(global.SalonAbierto()){
 		//Abrir Salon
 		int cliente = subasta.Accept();
 		thread th(&subastaCliente, subasta, cliente);
@@ -46,6 +55,7 @@ int main(int argc, char * argv[]){
 		// esperar a que puedan entrar más clientes a Subasta
 
 	}
+	global.finSubasta();
 	subasta.Close(sockSubasta);
 	
 	return 0;
