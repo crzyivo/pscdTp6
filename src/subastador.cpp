e24ba8105fec37976d101cb22c9a69b468237643
rankern <victorm.lafuente@gmail.com>
2018-01-03 11:12:10 +0100

    sub

--- src/subastador.cpp
+++ src/subastador.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <thread>
 #include "Socket.hpp"
-#include "monitorSubasta.hpp"
+#include "mSubasta.hpp"
 /***********************
  * Mensajes Servidor Cliente:
  * "Comienza la subasta en %d\n"
@@ -10,31 +10,32 @@
  * "Puja no valida. Actual puja %d %d \n", cliente, puja
  **********************/ 
 const int maxNumCLientes = 35;
-monitorSubasta global();
+monitorSubasta mSubas;
 
 int puertoSubasta = 32005;
 
-void subastaCliente(Socket& subasta, int cliente){
-	while(/*Condicion cliente desconectado*/){
+void subastaCliente(Socket *subasta, int cliente){
+	while(1/*Condicion cliente desconectado*/){
 		string mensajeOut = "";
 		string mensajeIn = "";
-		int precioInicial = global.comenzarSubasta();
+		int precioInicial = mSubas.comenzarSubasta();
 		mensajeOut = "Comienza la subasta en " + to_string(precioInicial) + "\n";
-		subasta.Send(cliente, mensajeOut);
+		subasta->Send(cliente, mensajeOut);
 		//esperar a que se dé la condición de iniciar subasta
 		while(1/*Condición de fin subasta Actual*/){
-			subasta.Recv(cliente, mensajeIn);
+			subasta->Recv(cliente, mensajeIn,1000);
+			int pujaCliente;
 			//leer mensaje
-			if(global.pujar(pujaCliente, cliente)){
-				mensajeOut = "Puja cliente " + to_string(cliente) + " aceptada. Quien ofrece\n" + to_string(global.pujaActual()+10) + "\n";
+			if(mSubas.pujar(pujaCliente, cliente)){
+				mensajeOut = "Puja cliente " + to_string(cliente) + " aceptada. Quien ofrece\n" + to_string(mSubas.pujaActual()+10) + "\n";
 			}else{
-				mensajeOut = "Puja no valida. Actual puja " + to_string(global.PujadorActual()) + to_string(global.pujaActual()) + "\n";
+				mensajeOut = "Puja no valida. Actual puja " + to_string(mSubas.PujadorActual()) + to_string(mSubas.pujaActual()) + "\n";
 			}
 			mensajeIn = "";
-			subasta.Recv(cliente, mensajeIn);
+			subasta->Recv(cliente, mensajeIn,10000);
 		}
 	}
-	int error = subasta.Close(cliente);
+	int error = subasta->Close(cliente);
 	if(error < 0){
 		printf("error al desconectar cliente\n");
 	}
@@ -47,15 +48,15 @@ int main(int argc, char * argv[]){
 	int sockSubasta = subasta.Bind();
 	int error = subasta.Listen(maxNumCLientes);
 
-	while(global.SalonAbierto()){
+	while(mSubas.SalonAbierto()){
 		//Abrir Salon
 		int cliente = subasta.Accept();
-		thread th(&subastaCliente, subasta, cliente);
-		th.detach();
+		thread th(&subastaCliente, &subasta,cliente);
+		//th.detach();
 		// esperar a que puedan entrar más clientes a Subasta
 
 	}
-	global.finSubasta();
+	mSubas.finSubasta();
 	subasta.Close(sockSubasta);
 	
 	return 0;
