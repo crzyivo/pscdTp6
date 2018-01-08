e31b2ff8a9b2124a33bd25350b1de38287904c87
rankern <victorm.lafuente@gmail.com>
2018-01-08 18:58:06 +0000

    am

--- src/Cliente.cpp
+++ src/Cliente.cpp
@@ -18,12 +18,12 @@ using namespace std;
 const int MESSAGE_SIZE = 4001; //mensajes de no más 4000 caracteres
 
 int main(int argc, char *argv[]) {
-    srand(time(0));
-    const string MENS_FIN("END OF SERVICE");
-    // Dirección y número donde escucha el proceso servidor
-    string SERVER_ADDRESS = "localhost";
-    string SERVER_PORT = "32005";
-    	string dir;
+	srand(time(0));
+	const string MENS_FIN("END OF SERVICE");
+	// Dirección y número donde escucha el proceso servidor
+	string SERVER_ADDRESS = "localhost";
+	string SERVER_PORT = "32005";
+		string dir;
 		if(argc >1){	//Inicializa con Parametros
 		for (int i = 1; i< argc; i++){
 			if(*argv[i]++ == '-'){
@@ -42,45 +42,45 @@ int main(int argc, char *argv[]) {
 			}
 		}
 	}
-    // cout<<"Introduce la direccion servidor: \n";
-    // getline(cin,SERVER_ADDRESS);
-    // cout<<"Introduce el numero de puerto: \n";
-    // getline(cin,SERVER_PORT);
-    string imagenes[7]={
-        "http://i.imgur.com/eTuCPxM.jpg",
-        "http://i.imgur.com/evzIQVF.jpg",
-        "http://i.imgur.com/BiFkD83.jpg",
-        "http://i.imgur.com/ApuihUx.jpg",
-        "http://i.imgur.com/LUiR7W8.jpg",
-        "http://i.imgur.com/5NljFvC.jpg",
-        "http://i.imgur.com/EygIdZU.jpg"};
-    
-    // Creación del socket con el que se llevará a cabo
-    // la comunicación con el servidor.
-    Socket socket(SERVER_ADDRESS,stoi(SERVER_PORT));
-    while(true){
-        // Conectamos con el servidor. Probamos varias conexiones
-        const int MAX_ATTEMPS = 10;
-        int count = 0;
-        int socket_fd;
-        do {
-            // Conexión con el servidor
-            socket_fd = socket.Connect();
-            count++;
-            
-            // Si error --> esperamos 1 segundo para reconectar
-            if(socket_fd == -1){
-                this_thread::sleep_for(chrono::seconds(1));
-            }
-        } while(socket_fd == -1 && count < MAX_ATTEMPS);
-        
-        // Chequeamos si se ha realizado la conexión
-        if(socket_fd == -1){
-            return socket_fd;
-        }
-        
-        
-        
+	// cout<<"Introduce la direccion servidor: \n";
+	// getline(cin,SERVER_ADDRESS);
+	// cout<<"Introduce el numero de puerto: \n";
+	// getline(cin,SERVER_PORT);
+	string imagenes[7]={
+		"http://i.imgur.com/eTuCPxM.jpg",
+		"http://i.imgur.com/evzIQVF.jpg",
+		"http://i.imgur.com/BiFkD83.jpg",
+		"http://i.imgur.com/ApuihUx.jpg",
+		"http://i.imgur.com/LUiR7W8.jpg",
+		"http://i.imgur.com/5NljFvC.jpg",
+		"http://i.imgur.com/EygIdZU.jpg"};
+	
+	// Creación del socket con el que se llevará a cabo
+	// la comunicación con el servidor.
+	Socket socket(SERVER_ADDRESS,stoi(SERVER_PORT));
+	while(true){
+		// Conectamos con el servidor. Probamos varias conexiones
+		const int MAX_ATTEMPS = 10;
+		int count = 0;
+		int socket_fd;
+		do {
+			// Conexión con el servidor
+			socket_fd = socket.Connect();
+			count++;
+			
+			// Si error --> esperamos 1 segundo para reconectar
+			if(socket_fd == -1){
+				this_thread::sleep_for(chrono::seconds(1));
+			}
+		} while(socket_fd == -1 && count < MAX_ATTEMPS);
+		
+		// Chequeamos si se ha realizado la conexión
+		if(socket_fd == -1){
+			return socket_fd;
+		}
+		
+		
+		
 //         string mensaje;
 //         string tvalla;
 //         int tam = rand()%2;
@@ -112,41 +112,44 @@ int main(int argc, char *argv[]) {
 //             cout << "Mensaje enviado: '" << mensaje << "'" << endl;
 //             cout << "Peticion: " << buffer << endl;
 //         }
-        cout<<"He conectado\n";
-        string inicio_puja="";
-        string puja = "";
-        string nueva_puja="";
-        
-        int read_bytes;
-        
-        read_bytes = socket.Recv(socket_fd,inicio_puja, MESSAGE_SIZE);
-        if(read_bytes == -1){
-          cerr << "Error al enviar datos: " << strerror(errno) << endl;
-          // Cerramos el socket
-          socket.Close(socket_fd);
-        }
-        
-        cout << inicio_puja << endl;
-        while(true){
-          cin >> puja;
-          if(puja == "p"){
-              read_bytes = socket.Send(socket_fd,"Paso\n");
-          }else if(puja == "a"){
-              read_bytes = socket.Send(socket_fd,"Acepto\n");
-        
-          }else{
-              cout<<"No te he entendido"<<endl;
-          }
-          read_bytes = socket.Recv(socket_fd,nueva_puja,MESSAGE_SIZE);
-          cout<<nueva_puja;
-        }
-        
-        
-        // Cerramos el socket
-        int error_code = socket.Close(socket_fd);
-        if(error_code == -1){
-            cerr << "Error cerrando el socket: " << strerror(errno) << endl;
-        }
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-    }
+		cout<<"He conectado\n";
+		string inicio_puja="";
+		string puja = "";
+		string nueva_puja="";
+		
+		int read_bytes;
+		
+		read_bytes = socket.Recv(socket_fd,inicio_puja, MESSAGE_SIZE);
+		if(read_bytes == -1){
+		  cerr << "Error al enviar datos: " << strerror(errno) << endl;
+		  // Cerramos el socket
+		  socket.Close(socket_fd);
+		}
+		
+		cout << inicio_puja << endl;
+		while(true){
+		  cin >> puja;
+		  if(puja == "p"){
+			  read_bytes = socket.Send(socket_fd,"Paso\n");
+		  }else if(puja == "a"){
+			  read_bytes = socket.Send(socket_fd,"Acepto\n");
+		  }else if(puja == "ss"){
+			read_bytes = socket.Send(socket_fd,"Salir de subasta\n");
+		  }else if(puja == "sp"){	
+			read_bytes = socket.Send(socket_fd,"Salir puja Actual\n");
+		  }else{
+			  cout<<"No te he entendido"<<endl;
+		  }
+		  read_bytes = socket.Recv(socket_fd,nueva_puja,MESSAGE_SIZE);
+		  cout<<nueva_puja;
+		}
+		
+		
+		// Cerramos el socket
+		int error_code = socket.Close(socket_fd);
+		if(error_code == -1){
+			cerr << "Error cerrando el socket: " << strerror(errno) << endl;
+		}
+		std::this_thread::sleep_for(std::chrono::seconds(2));
+	}
 }
